810ee4224e3ce1d46148e10a4b52f8205e3d4190
Gabo Vlad <gv.valdivieso2309@gmail.com>
2020-07-21 10:55:58 -0500

    Final proyecto

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -1,30 +1,38 @@
-import React from 'react';
+import React, {useState,useEffect} from 'react';
 import Header from '../components/Header'
 import Search from '../components/Search'
 import Categories from '../components/Categories'
 import Carousel from '../components/Carousel'
 import CarouselItem from '../components/CarouselItem'
 import Footer from '../components/Footer'
+import useInitialState from '../hooks/useInitialState'
 
 import '../assets/styles/App.scss'
 
-const App = () => (
+const API = 'http://localhost:3000/initalState'
+const App = () => {
+
+    const initialState =useInitialState(API)
+
+    return(
     <div className="App">
         <Header />
         <Search/>
-
-        <Categories title='Mi lista'>
-            <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
-                <CarouselItem/>
-            </Carousel>
-        </Categories>
+        {initialState.mylist.length > 0 &&    
+            <Categories title='Mi lista'>
+                <Carousel>
+                    <CarouselItem/>
+                </Carousel>
+             </Categories>
+        }
+        
 
         <Categories title='Tendencias'>
             <Carousel>
-                <CarouselItem/>
-                <CarouselItem/>
+                {initialState.trends.map(item  =>
+                    <CarouselItem key={item.id} {...item}/>
+                )}
+                
             </Carousel>
         </Categories>
 
@@ -36,5 +44,6 @@ const App = () => (
         </Categories>
         <Footer/>
     </div>
-)
+    )
+}
 export default App
